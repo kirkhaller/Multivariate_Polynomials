6bc5339c87e7c8a808cbe049f99cbcd6a2accc14
Kirk Haller <kirkhaller@gmail.com>
2020-06-27 17:30:52 -0400

    Added Google Tests

---
+++ GoogleTest/MultiindexTests.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Kirk Haller on 6/27/20.
+//
+
+#include "gtest/gtest.h"
+#include "../Multiindex.h"
+
+namespace {
+
+class MultiindexTests : public ::testing::Test {
+
+};
+
+} //namespace
---
+++ GoogleTest/PointTests.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Kirk Haller on 6/27/20.
+//
+
+#include "gtest/gtest.h"
+#include "../point.h"
+
+namespace {
+
+class PointTests :public ::testing::Test {
+
+
+
+};
+
+}
