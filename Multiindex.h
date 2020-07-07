9573381c786e30d1f9e99f33fb7fe843c6e430d0
Kirk Haller <kirkhaller@gmail.com>
2020-07-07 16:55:55 -0400

    Added a compare function for Multiindex to allow it to be appropriately used as a key in a map.

--- Multiindex.h
+++ Multiindex.h
@@ -5,6 +5,7 @@
 #ifndef MULTIVARIATE_POLYNOMIALS_MULTIINDEX_H
 #define MULTIVARIATE_POLYNOMIALS_MULTIINDEX_H
 
+#include <cassert>
 #include <string>
 
 class Multi_index {
@@ -25,6 +26,18 @@ inline bool operator==(const Multi_index &lhs, const Multi_index &rhs) {
 
 inline bool operator!=(const Multi_index &lhs, const Multi_index &rhs) {
     return !(lhs == rhs);
+}
+
+inline bool operator<(const Multi_index &lhs, const Multi_index &rhs) {
+    assert(lhs.dimension() == rhs.dimension() && "Mismatched Dimensions.");
+    for (int index = 0; index < lhs.dimension(); index++) {
+        int lhs_value = lhs.get_value(index);
+        int rhs_value = rhs.get_value(index);
+        if (lhs_value != rhs_value) {
+            return lhs_value < rhs_value;
+        }
+    }
+    return false; // The operators must be equal
 }
 
 #endif //MULTIVARIATE_POLYNOMIALS_MULTIINDEX_H
